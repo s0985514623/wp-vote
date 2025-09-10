6c3300d492d6b58a4aba66a502caee884e5942df
Ren <s0985514623@gmail.com>
2025-09-10 13:41:46 +0800

    chore: 更新发布插件名称为 wp-vote-release

--- release/.release-it.cjs
+++ release/.release-it.cjs
@@ -11,7 +11,7 @@
  * @see https://github.com/release-it/release-it/blob/main/docs/configuration.md
  */
 
-const releasedPluginName = 'wp-vote'
+const releasedPluginName = 'wp-vote-release'
 
 module.exports = {
   releasedPluginName,
