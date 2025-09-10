d31b9fab3a66b32d42d228333fcf600d64258c7d
Ren <s0985514623@gmail.com>
2025-09-10 12:06:09 +0800

    chore: release v4.0.5

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WP Vote | 民調投票系統
  * Plugin URI:        https://github.com/s0985514623
  * Description:       民調投票系統
- * Version:           1.0.0
+ * Version:           4.0.5
  * Requires at least: 5.7
  * Requires PHP:      8.
  * Author:            s0985514623
